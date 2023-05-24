e761a868e8543bb937f19f40820c9a19e45824e8
1shadowpy <132641739+1shadowpy@users.noreply.github.com>
2023-05-24 11:12:05 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,62 @@
+# sinf ={}
+# sinf['oquvchi']= 'dolimop'
+# sinf['rahbar']='karimov'
+# sinf['direktor']= 'shavkat'
+# sinf ['sinif - oquvchilar-soni']= 999999999999990999999999999999
+# sinf ['darslar']='matem'
+# print(f"mening sinifmda {sinf['oquvchi']} oqiydi , \n mening sinifimning rahbari {sinf['rahbar']} , \n maning maktabimni derektori bu {sinf['direktor']} , \n sinifimning - oquvchilar-soni {sinf['sinif - oquvchilar-soni']} ,\n bizning sinf sonimiz darsi {sinf['darslar']}")
+# for k,q in sinf.items():
+#     print(f"{k},{q}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
